aaf49db838cdf4a1370f95b786f9a5f3f8a79dfb
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2023-02-12 18:23:08 +0300

    Create home screen

---
+++ src/screens/Home.js
@@ -0,0 +1,11 @@
+import { View, Text, TextInput, SafeAreaView, StyleSheet, TouchableOpacity, Dimensions } from 'react-native'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+export default Home
